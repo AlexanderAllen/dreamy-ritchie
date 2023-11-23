21da6bfbb327ae3c93da60f30f8a3efcd5fc04b5
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-11-23 13:22:01 -0500

    Use filesystem service to get spec file location.

--- docroot/web/modules/custom/musica/src/Spec/YamlParametersTrait.php
+++ docroot/web/modules/custom/musica/src/Spec/YamlParametersTrait.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\musica\Spec;
 
+use Drupal\Core\Extension\ExtensionList;
+use Drupal\Core\File\FileSystem;
 use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
@@ -11,16 +13,33 @@ use Symfony\Component\Yaml\Yaml;
 trait YamlParametersTrait {
 
   /**
+   * Static ExtensionList service.
+   */
+  public function extensionList(): ExtensionList {
+    return \Drupal::service('extension.list.module');
+  }
+
+  /**
+   * Static Filesystem service.
+   */
+  public function filesystem(): FileSystem {
+    return \Drupal::service('file_system');
+  }
+
+  /**
    * Provides method parameters defined in yaml files.
    *
    * @return array
    *   Array of method parameters.
    */
   public function parameters(): array {
+    $module_path = $this->extensionList()->getPath('musica');
+    $real_path = $this->filesystem()->realpath($module_path);
+    $spec_file = $real_path . '/src/Spec/LastFM/file.yaml';
 
     try {
       // @todo use drupal filesystem services for relative file resolution.
-      $parsed_spec = Yaml::parseFile('/app/file.yaml', Yaml::PARSE_CONSTANT);
+      $parsed_spec = Yaml::parseFile($spec_file, Yaml::PARSE_CONSTANT);
     } catch (ParseException $exception) {
       printf('Unable to parse the YAML string: %s', $exception->getMessage());
     }
