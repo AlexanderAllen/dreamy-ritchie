6952e83afa051684dfb337c8868ec1af189cbc85
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-11-23 11:09:17 -0500

    Initial YamlParameters trait.

---
+++ docroot/web/modules/custom/musica/src/Spec/YamlParameters.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\musica\Spec;
+
+use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Trait for matching object names to yaml file keys.
+ */
+trait YamlParameters {
+
+  /**
+   * Provides method parameters defined in yaml files.
+   *
+   * @return array
+   *   Array of method parameters.
+   */
+  public function parameters(): array {
+
+    try {
+      // @todo use drupal filesystem services for relative file resolution.
+      $parsed_spec = Yaml::parseFile('/app/file.yaml', Yaml::PARSE_CONSTANT);
+    } catch (ParseException $exception) {
+      printf('Unable to parse the YAML string: %s', $exception->getMessage());
+    }
+    return $parsed_spec[$this->name] ??= NULL;
+  }
+
+}
