2aa4782388776e6e1a4f61190cc1a72155c6f80f
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-11-24 22:41:47 -0500

    Semantics.

--- docroot/web/modules/custom/musica/src/Controller/HelloController.php
+++ docroot/web/modules/custom/musica/src/Controller/HelloController.php
@@ -47,7 +47,31 @@ class HelloController extends ControllerBase {
 
     // $i2 = ServiceContainer::entity(new Entity('Cher'))->getInfo('bio');
 
-    $i = ServiceContainer::entity(new Entity('<h1> Cher </h1>'))->map('htmlspecialchars')->map('strtolower') ;
+    // second iteration, using custom objects, Entity in this case, instead of native scalars.
+    $i = EntityContainer::create(new Entity('<h1> Cher </h1>'))->map('htmlspecialchars')->map('strtolower') ;
+
+    // 3rd iteration - wrap map with magic __call method.
+    // 3.1 iteration - safe map wrap - returns a safe and sound entity if the method is not found,
+    //    allows the chain to continue execution, maybe log into an internal object array errors.
+    // 4th iteration - populate/transform entity with information from various api calls.
+    // ...
+    // 5th iteration - standarization - make one state entity to use across services, MUST USE INTERFACE
+    // ...
+    // 6th-8th iterations - pass and manipulate the entity between various containers.
+
+    // some other ideas for further iteration
+    // how about ServiceContainer::artist('Cher) -> creates artist entity out of a base shareable entity
+    // so ServiceContainer::artist('Cher')->getInfo()->getBio() // getBio could also call the first if data not populated.
+    // Also this container is more like an entity container
+    // perhaps is best to have a separate service container, that can contain the entity container
+    // so...
+    // ServiceContainerLastFM::EntityContainer::entity->method1()->method2;
+    // LastFM (implements ServiceContainer)::Artist (EntityContainer) :: entity->method1
+    // LastFM::Artist::method1
+    // or
+    //
+
+
 
     // Retrieve API call parameters from spec file.
     $spec = artist::getInfo->parameters();
@@ -74,37 +98,13 @@ class HelloController extends ControllerBase {
 
 }
 
-// Think of Entity as a string... they're both objects, this one is custom.
-class Entity {
-  private $name;
-
-  public function __construct($name) {
-    $this->name = $name;
-  }
-
-  public function getInfo($args) {
-    $this->name = "Here is some info for {$this->name}" . $args[0];
-  }
-
-  public function htmlspecialchars() {
-    $this->name = htmlspecialchars($this->name);
-  }
-
-  public function strtolower() {
-    $this->name = strtolower($this->name);
-  }
-
-  public function __invoke() {
-    return new static($this->name);
-  }
-}
 
 // i guess we could still transform the entity by populating it or performing actions on it
 // but always returning the same object so it remains chainable.
 // $i = ServiceContainer::entity(new Entity('Cher'))->map('getInfo');
 
 // instead of map, __call the api method directly, chainable too?
-class ServiceContainer {
+class EntityContainer {
   private Entity $object;
 
   private function __construct(Entity $object) {
@@ -112,7 +112,7 @@ class ServiceContainer {
   }
 
   // Unit function
-  public static function entity(Entity $obj) {
+  public static function create(Entity $obj) {
      return new static($obj);
   }
 
@@ -140,5 +140,30 @@ class ServiceContainer {
 
   public function __call($f, $args = []) {
     $this->map($f, $args);
+  }
+}
+
+// Think of Entity as a string... they're both objects, this one is custom.
+class Entity {
+  private $name;
+
+  public function __construct($name) {
+    $this->name = $name;
+  }
+
+  public function getInfo($args) {
+    $this->name = "Here is some info for {$this->name}" . $args[0];
+  }
+
+  public function htmlspecialchars() {
+    $this->name = htmlspecialchars($this->name);
+  }
+
+  public function strtolower() {
+    $this->name = strtolower($this->name);
+  }
+
+  public function __invoke() {
+    return new static($this->name);
   }
 }
