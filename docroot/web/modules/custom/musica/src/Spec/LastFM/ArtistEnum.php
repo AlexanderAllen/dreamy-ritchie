a6fe3be6b8e1d753d4df99670025e7a0ff0bc097
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-11-25 22:09:03 -0500

    Remove behaviors from artist enum.

--- docroot/web/modules/custom/musica/src/Spec/LastFM/ArtistEnum.php
+++ docroot/web/modules/custom/musica/src/Spec/LastFM/ArtistEnum.php
@@ -2,15 +2,10 @@
 
 namespace Drupal\musica\Spec\LastFM;
 
-use Drupal\musica\Spec\LastFM\YamlParametersLastFMTrait;
-use Drupal\musica\Spec\YamlParametersTrait;
-
 /**
  * Enumerates artist API methods available, with parameters.
  */
 enum ArtistEnum {
-  use YamlParametersTrait;
-  use YamlParametersLastFMTrait;
 
   case addTags;
   case getCorrection;
@@ -22,12 +17,4 @@ enum ArtistEnum {
   case getTopTracks;
   case removeTag;
   case search;
-
-  /**
-   * Return API namespace name.
-   */
-  public function __invoke() {
-    return 'artist';
-  }
-
 }
