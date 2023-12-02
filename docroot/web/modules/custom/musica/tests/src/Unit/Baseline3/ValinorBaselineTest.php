a24ce1fe06e41be55653423b164555422a3879ff
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-12-02 02:47:27 -0500

    Initial baseline 3.
    Confirmed the # property causes Valinor to crash.

---
+++ docroot/web/modules/custom/musica/tests/src/Unit/Baseline3/ValinorBaselineTest.php
@@ -0,0 +1,82 @@
+<?php
+
+// phpcs:disable Drupal.Commenting.DocComment.ContentAfterOpen
+
+namespace Drupal\Tests\musica\Unit\Baseline3;
+
+use Cuyz\Valinor\Mapper\MappingError;
+use CuyZ\Valinor\MapperBuilder;
+use CuyZ\Valinor\Mapper\Source\Source;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Basic Valinor DTO hydration test.
+ *
+ * @group musica
+ * @see https://github.com/CuyZ/Valinor#example
+ */
+class ValinorBaselineTest extends TestCase {
+
+  public function testSimilarArtistsWithoutAttr() {
+    // $response = Source::json($json);
+    $sauce = Source::file(new \SplFileObject('/app/docroot/web/modules/custom/musica/tests/src/Unit/Baseline3/similarartists.json'));
+
+    try {
+      $dto = (new MapperBuilder())
+        ->mapper()
+        ->map(Similar::class, $sauce);
+
+      $this->assertSame('France', 'France');
+
+    }
+    // @phpstan-ignore-next-line
+    catch (MappingError $error) {
+      $e = $error;
+    }
+
+  }
+
+}
+
+
+final class Similar {
+
+  public function __construct(
+    /** @var non-empty-string */
+    public readonly string $name,
+    /** @var list<Artist> */
+    public readonly array $artists,
+  ) {}
+
+}
+
+
+final class Artist {
+
+  public function __construct(
+    /** @var non-empty-string */
+    public readonly string $name,
+    /** @var non-empty-string */
+    public readonly string $mbid,
+    /** @var non-empty-string */
+    public readonly string $match,
+    /** @var non-empty-string */
+    public readonly string $url,
+    /** @var list<Image> */
+    public readonly array $image,
+    /** @var non-empty-string */
+    public readonly string $streamable,
+  ) {}
+
+}
+
+final class Image {
+
+  public function __construct(
+    /** @var non-empty-string */
+    public readonly string $text,
+    /** @var non-empty-string */
+    public readonly string $size,
+  ) {}
+
+}
