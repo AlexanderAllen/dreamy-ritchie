b4721efb9636c7dfba0885ce332f1ad2c20b85d0
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-12-04 02:43:17 -0500

    Baseline7 unit test.

--- docroot/web/modules/custom/musica/tests/src/Unit/Baseline7/HydrateGenericsTest.php
+++ docroot/web/modules/custom/musica/tests/src/Unit/Baseline7/HydrateGenericsTest.php
@@ -6,7 +6,7 @@
 // phpcs:disable Squiz.WhiteSpace.FunctionSpacing.BeforeFirst, Drupal.Classes.ClassDeclaration.CloseBraceAfterBody
 // phpcs:disable Drupal.Classes.FullyQualifiedNamespace.UseStatementMissing
 
-namespace Drupal\Tests\musica\Unit\LFM\Artist\GetTopAlbums;
+namespace Drupal\Tests\musica\Unit\Baseline7;
 
 use CuyZ\Valinor\MapperBuilder;
 use CuyZ\Valinor\Mapper\Source\Source;
@@ -16,14 +16,17 @@ use PHPUnit\Framework\TestCase;
  * Basic Valinor DTO hydration test.
  *
  * @group musica
- * @group ignnore
  *
  * @see https://www.last.fm/api/show/artist.getTopAlbums
  * @see https://github.com/CuyZ/Valinor#example
  * @see https://valinor.cuyz.io/1.7/how-to/use-custom-object-constructors
  * @see https://valinor.cuyz.io/1.7/usage/type-strictness-and-flexibility/
  *
- * For information on generics.
+ * For PHPStan and Valinor information on generics.
+ * @see https://github.com/CuyZ/Valinor/issues/8 valinor generics
+ * @see https://github.com/CuyZ/Valinor/blob/396f64a5246ccfe3f6f6d3211bac7f542a9c7fc6/README.md#object
+ * @see https://phpstan.org/blog/generics-in-php-using-phpdocs
+ * @see https://phpstan.org/blog/generics-by-examples
  */
 class HydrateGenericsTest extends TestCase {
 
@@ -41,17 +44,70 @@ class HydrateGenericsTest extends TestCase {
         ->allowPermissiveTypes()
         // ->enableFlexibleCasting()
         ->mapper()
-        ->map($signature, $sauce);
+        // ->map($signature, $sauce);
+        ->map(MyGenericWrapper::class . '<string>', ['value' => 'who knows?']);
 
       $this->assertSame(TRUE, TRUE);
     }
     catch (\CuyZ\Valinor\Mapper\MappingError $error) {
-      Debugger::toCLI($error);
+      // Debugger::toCLI($error);
+      $test = NULL;
     }
 
   }
 
 }
+
+// phpcs:disable
+/**
+ * @template T of mixed
+ */
+class MyGenericWrapper
+{
+    /** @var T */
+    public mixed $value;
+}
+
+/**
+ * @template T of mixed
+ * …or…
+ * @template T // mixed is the default type
+ *
+ * You can also use advanced types:
+ * @template T of int|float
+ * @template T of SomeClass&SomeOtherClass
+ * @template T of string|SomeClass
+ * etc…
+ */
+class MyGenericWrapperOG
+{
+    /** @var T */
+    public mixed $value;
+}
+
+
+/**
+ * @template T of Tag
+ */
+final class GenericEntityList {
+
+  public function __construct(
+    /** @var T */
+    public $list,
+  ) {
+    $test = null;
+  }
+}
+
+
+final class Tag {
+  public function __construct(
+    public readonly int $count,
+    public readonly string $name,
+    public readonly string $url = '',
+  ) {}
+}
+
 
 class ImageProps {
   public readonly string $text;
@@ -108,9 +164,6 @@ class Attribute {
 
 }
 
-/**
- * @see https://phpstan.org/writing-php-code/phpdoc-types#local-type-aliases
- */
 final class Artist {
 
   public function __construct(
