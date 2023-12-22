f786f2338014ac475a5c9b6551413e51cbe74ded
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2023-12-21 19:43:45 -0500

    Pass data into the view result.

--- docroot/web/modules/custom/musica_views/src/EventSubscriber/MyRemoteDataSubscriber.php
+++ docroot/web/modules/custom/musica_views/src/EventSubscriber/MyRemoteDataSubscriber.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Drupal\musica_views\EventSubscriber;
 
 use Drupal\musica\Service\Spotify;
-use Drupal\Component\Serialization\Json;
 use Drupal\views\ResultRow;
 use Drupal\views_remote_data\Events\RemoteDataQueryEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -44,13 +43,8 @@ final class MyRemoteDataSubscriber implements EventSubscriberInterface {
     $base_tables = array_keys($event->getView()->getBaseTables());
     if (count(array_intersect($supported_bases, $base_tables)) > 0) {
 
-      // remote service call.
       $data = $this->spotify->getResourceObject();
-
-      // $remote_data = $this->methodWhichCallsRemoteApi();
-      // foreach ($remote_data['data'] as $item) {
-      //     $event->addResult(new ResultRow($item));
-      // }
+      $event->addResult(new ResultRow(['data' => $data]));
     }
   }
 
