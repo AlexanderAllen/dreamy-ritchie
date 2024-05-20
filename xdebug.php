44663c18760f04472951232e253809228fb908c1
AlexanderAllen <14018885+AlexanderAllen@users.noreply.github.com>
2024-05-20 06:22:42 -0400

    Helper debug script.

---
+++ xdebug.php
@@ -0,0 +1,14 @@
+<?php
+
+# To test out that debugging is workign, do the following.
+#
+# (should lando you in this directory)
+# lando ssh
+
+# export the following two vars
+# export XDEBUG_MODE="debug"
+# export XDEBUG_CONFIG="idekey=VSCODE"
+
+# then run php hello.php
+
+echo "hello world";
